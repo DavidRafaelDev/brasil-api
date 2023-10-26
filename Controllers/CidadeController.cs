10ba14d82c5b80c7810d113c900de846730b0bec
davidrafaeldev <davidrafaelmsm3@gmail.com>
2023-10-26 12:52:07 -0300

    first commit

---
+++ Controllers/CidadeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.Controllers
+{
+    [ApiController]
+    [Route("cidade")]
+    public class CidadeController : ControllerBase
+    {
+        
+        [HttpGet("aeroporto/{aeroporto}")]
+        public async Task<IActionResult> GetClimaAeroporto(string aeroporto)
+        {
+            var client = new HttpClient();
+            var url = $"https://brasilapi.com.br/api/cptec/v1/clima/aeroporto/{aeroporto}";
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                return Ok(data);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, "Erro ao buscar dados do clima do aeroporto.");
+            }
+        }
+    }
+}
